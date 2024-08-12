d8c78a93fb6be82f89ccecfe7a77a07402a81a76
vlwmv <vl@key-info.com.ua>
2024-08-12 20:51:09 +0300

    Update...

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ async def favicon():
 
 # Create (POST)
 @app.post("/users/", response_model=schemas.User, tags=["Users"])
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db),
+                      permission: bool = Depends(auth.RBAC(acl=["admin", "manager"]))):
     # Check if unique User's identification attributes already exists
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -91,11 +92,8 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 # Read (GET)
 @app.get("/users/{user_id}", response_model=schemas.User, tags=["Users"])
-async def read_user(
-        user_id: int,
-        db: Session = Depends(get_db),
-        permission: bool = Depends(auth.RBAC(acl=["admin", "users:read"]))
-):
+async def read_user(user_id: int, db: Session = Depends(get_db),
+                    permission: bool = Depends(auth.RBAC(acl=["admin", "manager", "users:read"]))):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found!")
@@ -104,7 +102,8 @@ async def read_user(
 
 # Update (PUT)
 @app.put("/users/{user_id}", response_model=schemas.User, tags=["Users"])
-async def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db),
+                      permission: bool = Depends(auth.RBAC(acl=["admin", "manager"]))):
     # Check if User exists
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -115,7 +114,8 @@ async def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depe
 
 # Delete (DELETE)
 @app.delete("/users/{user_id}", tags=["Users"])
-async def delete_user(user_id: int, db: Session = Depends(get_db)):
+async def delete_user(user_id: int, db: Session = Depends(get_db),
+                      permission: bool = Depends(auth.RBAC(acl=["admin", "manager"]))):
     # Check if User exists
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
