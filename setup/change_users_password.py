7703b328ea80fca7e99e3277764d0b417fe8a3c1
vlwmv <vl@key-info.com.ua>
2024-08-29 20:10:21 +0300

    Update...

--- setup/change_users_password.py
+++ setup/change_users_password.py
@@ -1,11 +1,48 @@
-from sql_app import auth
-from util import get_setup
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from sql_app import models
+from sql_app.database import get_db
+from util import get_setup, get_config, get_current_time_utc
 
-SUCCESSFUL_MESSAGE = "Password for User user_name successfully updated:"
+APP_CONFIG = get_config()
+SUCCESSFUL_MESSAGE = "Password for Username 'user_name' successfully updated: "
+FAILURE_MESSAGE = "Password for Username 'user_name' NOT updated!"
+PWD_CONTEXT = CryptContext(schemes=APP_CONFIG["auth"]["PWD_CONTEXT"]["schemes"],
+                           deprecated=APP_CONFIG["auth"]["PWD_CONTEXT"]["deprecated"])
 
-for user in get_setup()["users"]:
-    username = user["username"]
-    plain_password = user["password"]
-    hashed_password = auth.get_password_hash(plain_password)
 
-    print(SUCCESSFUL_MESSAGE.replace("user_name", username) + ": " + plain_password)
+def get_password_hash(plain_password: str) -> str:
+    return PWD_CONTEXT.hash(plain_password)
+
+
+def get_user_by_name(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def update_users_passwords(db: Session = get_db):
+
+    for user in get_setup()["users"]:
+        username = user["username"]
+        plain_password = user["password"]
+        hashed_password = get_password_hash(plain_password)
+
+        # Check if User exists
+        db_user = get_user_by_name(db=db, username=username)
+        if db_user is None:
+            print(FAILURE_MESSAGE)
+        else:
+            # Set new password
+            db_user.hashed_password = hashed_password
+
+            # Set update time-date
+            db_user.updated = get_current_time_utc("TIME")
+
+            # Update database
+            db.commit()
+            db.refresh(db_user)
+
+            print(SUCCESSFUL_MESSAGE.replace("user_name", username) + plain_password)
+
+
+# calling next() on your generator to get a session out of the generator.
+update_users_passwords(db=next(get_db()))
--- sql_app/database.py
+++ sql_app/database.py
@@ -11,31 +11,16 @@ SQLALCHEMY_DATABASE_URL = f"sqlite:////{util.get_project_root()}{util.get_config
 
 # connect_args is needed only for SQLite. It's not needed for other databases!
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
 # Dependency -> We need to have an independent database session/connection (SessionLocal) per request, use the same
 # session through all the request and then close it after the request is finished. And then a new session will be
 # created for the next request.
-#
-# Also, we can add "Alternative DB session" with middleware function (with relevant + and -)
-# A "middleware" is basically a function that is always executed for each request,
-# with some code executed before, and some code executed after the endpoint function.
-# https://fastapi.tiangolo.com/tutorial/sql-databases/#alternative-db-session-with-middleware
-# @app.middleware("http")
-# async def db_session_middleware(request: Request, call_next):
-#     response = Response("Internal server error", status_code=500)
-#     try:
-#         request.state.db = SessionLocal()
-#         response = await call_next(request)
-#     finally:
-#         request.state.db.close()
-#     return response
-#
-# # Dependency
-# def get_db(request: Request):
-#     return request.state.db
-def get_db():
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db() -> SessionLocal():
     db = SessionLocal()
     try:
         yield db
