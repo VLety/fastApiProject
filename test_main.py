c755ba6673fa184e0ba8557d986b0307d38c913e
vlwmv <vl@key-info.com.ua>
2024-09-09 15:20:12 +0300

    Update...

--- test_main.py
+++ test_main.py
@@ -6,100 +6,132 @@ License: MIT
 """
 from pytest_assert_utils import util as pt_util
 from fastapi.testclient import TestClient
-from main import app
+from main import app, APP_CONFIG
 import util
 
 TestApiServer = TestClient(app)
+api_root_path = APP_CONFIG["root_path"]
 
-valid_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInNjb3BlcyI6W10sImV4cCI6MTcyNTg3NzY5OX0.V6apnF1O_aT1ct_WoCanzWZSnncJGmTcnfKl7HZJTK4"
+valid_admin_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInNjb3BlcyI6W10sImV4cCI6MTcyNTg4Njk5Mn0._l-zoVntv746x7sS0aUkh8A4IL3krvsJV_Dj1bGOcjM"
 expired_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInNjb3BlcyI6W10sImV4cCI6MTcyNTg3MzIyNX0.BnQ0CRDKkKUzvlkkeF_J3_QeipIIR9BkuAQpoz7ZJ0Y"
-wrong_token = "Wrong Token!"
+wrong_token = "Wrong Token!!!"
 
-valid_headers = {"Authorization": "Bearer " + valid_token}
-expired_headers = {"Authorization": "Bearer " + expired_token}
-wrong_headers = {"Authorization": "Bearer " + wrong_token}
+valid_admin_header = {"accept": "application/json", "Authorization": "Bearer " + valid_admin_token}
+expired_header = {"accept": "application/json", "Authorization": "Bearer " + expired_token}
+wrong_header = {"accept": "application/json", "Authorization": "Bearer " + wrong_token}
+valid_manager_header = {"accept": "application/json", "Authorization": "Bearer "}
 
-current_user = {
-    "username": "admin",
-    "first_name": "David",
-    "last_name": "Bauer",
-    "phone": "+431279843271",
-    "email": "admin@fastapi.com",
-    "role": [
-        "admin"
-    ],
-    "disabled": False,
-    "login_denied": False,
-    "id": 0,
-    "created": "string",
-    "updated": "string"
-}
-
-new_employee = {
+new_manager_user = {
+    "username": "IAmManager",
     "first_name": "Jones",
     "last_name": "Smith",
+    "phone": "+380504430000",
+    "email": "user@gmail.com",
+    "role": [
+        "manager"
+    ],
+    "disabled": False,
+    "login_denied": False
+}
+new_manager_user_password = "passWord@8"
+
+new_employee = {
+    "first_name": "Marry",
+    "last_name": "Fox",
     "nick_name": "Bravo",
     "phone": "+380504430000",
     "email": "user@gmail.com",
-    "birthday": "2024-06-01",
+    "birthday": "1998-06-01",
     "country": "Ukraine",
     "city": "Kyiv",
-    "address": "Khreschatyk St, 14, UA 01001",
-    "id": 0,
-    "created": "string",
-    "updated": "string",
-    "tickets": []
+    "address": "Khreschatyk St, 14, UA 01001"
+}
+
+new_ticket = {
+    "title": "Network problem",
+    "description": "The employee cannot access network resources.",
+    "status": "New"
 }
 
 
-def test_read_me_wrong_token():
-    response = TestApiServer.get("/api/v1/me", headers=wrong_headers)
+def print_response(response):
     print("\n" + "Response:")
     util.print_json(response.json())
 
-    assert response.status_code == 401
+
+def test_read_me_wrong_token():
+    response = TestApiServer.get(api_root_path + "/me", headers=wrong_header)
+    print_response(response)
+
+    assert response.status_code == APP_CONFIG["raise_error"]["could_not_validate_credentials"]["status_code"]
     assert response.json() == {
-        "detail": "Could not validate credentials"
+        "detail": APP_CONFIG["raise_error"]["could_not_validate_credentials"]["detail"]
     }
 
 
 def test_read_me_expired_token():
-    response = TestApiServer.get("/api/v1/me", headers=expired_headers)
-    print("\n" + "Response:")
-    util.print_json(response.json())
+    response = TestApiServer.get(api_root_path + "/me", headers=expired_header)
+    print_response(response)
 
-    assert response.status_code == 400
+    assert response.status_code == APP_CONFIG["raise_error"]["token_has_expired"]["status_code"]
     assert response.json() == {
-        "detail": "Token has expired"
+        "detail": APP_CONFIG["raise_error"]["token_has_expired"]["detail"]
+    }
+
+
+def test_create_new_support_user():
+    # Add password
+    new_support_user_with_password = new_manager_user.copy()
+    new_support_user_with_password["password"] = new_manager_user_password
+
+    response = TestApiServer.post(api_root_path + "/user/",
+                                  headers=valid_admin_header,
+                                  json=new_support_user_with_password)
+    print_response(response)
+
+    new_manager_user["id"] = response.json()["id"]
+    new_manager_user["created"] = response.json()["created"]
+    new_manager_user["updated"] = response.json()["updated"]
+
+    assert response.status_code == 200
+    assert response.json() == new_manager_user
+
+
+def test_get_new_manager_user_token():
+    response = TestApiServer.post(api_root_path + "/token", data={
+        "username": new_manager_user["username"],
+        "password": new_manager_user_password
+    })
+    print_response(response)
+
+    valid_manager_header["Authorization"] = valid_manager_header["Authorization"] + response.json()["access_token"]
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "access_token": pt_util.Any(str),
+        "token_type": "bearer"
     }
 
 
 def test_read_me():
-    response = TestApiServer.get("/api/v1/me", headers=valid_headers)
-    print("\n" + "Response:")
-    util.print_json(response.json())
-
-    current_user["id"] = response.json()["id"]
-    current_user["created"] = response.json()["created"]
-    current_user["updated"] = response.json()["updated"]
+    response = TestApiServer.get(api_root_path + "/me", headers=valid_manager_header)
+    print_response(response)
 
     assert response.status_code == 200
-    assert response.json() == current_user
+    assert response.json() == new_manager_user
 
 
 def test_read_status():
-    response = TestApiServer.get("/api/v1/status", headers=valid_headers)
-    print("\n" + "Response:")
-    util.print_json(response.json())
+    response = TestApiServer.get(api_root_path + "/status", headers=valid_manager_header)
+    print_response(response)
 
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
 
 def test_create_employee():
-    response = TestApiServer.post("/api/v1/employee", headers=valid_headers, json=new_employee)
-    print("\n" + "Response:")
-    util.print_json(response.json())
+    response = TestApiServer.post(api_root_path + "/employee", headers=valid_manager_header, json=new_employee)
+    print_response(response)
 
     new_employee["id"] = response.json()["id"]
     new_employee["created"] = response.json()["created"]
